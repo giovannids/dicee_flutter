ecc2d980c119f0585ca69f4fcfca44ba34314804
Giovanni DS <g-io@MacBook-Air-de-Giovanni.local>
2021-07-01 09:59:09 -0600

    DCompleta diseño visual de la app

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,34 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          // A widget that expands a child of a Row, Column, or Flex so that the child fills the available space.
+          Expanded(
+            // A widget that insets its child by the given padding.
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              // Several constructors are provided for the various ways that an image can be specified:
+              // new Image.asset, for obtaining an image from an AssetBundle using a key.
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/dice1.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
+/*
+REFERENCIAS
+https://api.flutter.dev/flutter/widgets/Expanded-class.html
+https://api.flutter.dev/flutter/widgets/Image-class.html
+https://api.flutter.dev/flutter/widgets/Padding-class.html
+ */
